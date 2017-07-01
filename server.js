902d4084d5ef436f2114f48e1ead0d3eee3d9965
FernandoCagale <fernando.b.cagale@gmail.com>
2017-07-01 10:59:17 -0300

    add mesh

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const Seneca = require('seneca');
 
-Seneca({tag: 'api'})
-  .client({
-    pin: 'role:order,cmd:*',
-    port: 9001
-  })
-  .client({
-    pin: 'role:auth,cmd:*',
-    port: 9002
-  })
-  .client({
-    pin: 'role:invoice,cmd:*',
-    port: 9003
-  })
-  .ready(function () {
-    const seneca = this;
-    require('./src/core/bootstrap').start(seneca);
-  });
+Seneca({
+  tag: 'base'
+}).use('consul-registry', {
+  host: process.env.ADDR || '127.0.0.1'
+}).use('seneca-mesh', {
+  discover: {
+    registry: {
+      active: true
+    },
+    multicast: {
+      active: false
+    }
+  }
+}).ready(function () {
+  const seneca = this;
+  require('./src/core/bootstrap').start(seneca);
+});
