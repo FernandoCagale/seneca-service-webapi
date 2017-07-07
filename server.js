f28f3ee9c0b23377dfc539b728e35f65e8822cc9
FernandoCagale <fernando.b.cagale@gmail.com>
2017-07-07 19:38:57 -0300

    alter service

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const Seneca = require('seneca');
 
 Seneca({
-  tag: 'base'
+  tag: 'webapi'
 }).use('consul-registry', {
-  host: process.env.ADDR || '127.0.0.1'
+  host: process.env.CONSUL_ADDR || '127.0.0.1'
 }).use('seneca-mesh', {
+  listen: [{
+    host: process.env.ADDR || '127.0.0.1',
+    port: process.env.PORT || 50000 + Math.floor((10000 * Math.random()))
+  }],
   discover: {
     registry: {
       active: true
     },
     multicast: {
+      active: false
+    },
+    guess: {
       active: false
     }
   }
