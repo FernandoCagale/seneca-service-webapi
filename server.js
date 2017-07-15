ef29991409c56ebbdae420eedf43293c79c188df
FernandoCagale <fernando.b.cagale@gmail.com>
2017-07-15 10:20:05 -0300

    alter webapi

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-const Seneca = require('seneca');
+'use strict';
 
-Seneca({
-  tag: 'webapi'
-}).use('consul-registry', {
-  host: process.env.CONSUL_ADDR || '127.0.0.1'
-}).use('seneca-mesh', {
-  listen: [{
-    host: process.env.ADDR || '127.0.0.1',
-    port: process.env.PORT || 50000 + Math.floor((10000 * Math.random()))
-  }],
-  discover: {
-    registry: {
-      active: true
-    },
-    multicast: {
-      active: false
-    },
-    guess: {
-      active: false
+const seneca = require('seneca')({base: 'webapi'});
+const mesh = require('seneca-mesh');
+const consul = require('seneca-consul-registry');
+
+const opts = {
+  mesh: {
+    listen: [{
+      host: process.env.ADDR || '127.0.0.1',
+      port: process.env.PORT || 50000 + Math.floor((10000 * Math.random()))
+    }],
+    discover: {
+      registry: {
+        active: true
+      },
+      multicast: {
+        active: false
+      },
+      guess: {
+        active: false
+      }
     }
+  },
+  consul: {
+    host: process.env.CONSUL_ADDR || '127.0.0.1'
   }
-}).ready(function () {
-  const seneca = this;
-  require('./src/core/bootstrap').start(seneca);
+};
+
+seneca.use(consul, opts.consul);
+seneca.use(mesh, opts.mesh).ready(function () {
+  require('./src/core/bootstrap').start(this);
 });
